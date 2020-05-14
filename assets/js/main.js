983142015f21433de1580e5ae3b3c55805621f2a
Patrick Totzke <patricktotzke@gmail.com>
2020-05-14 19:02:32 +0100

    map: explain config options in example config

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,11 +4,6 @@
 {% include js/conference.js %}
 
 (function() {
-    // Alternative map providers can be found on https://leaflet-extras.github.io/leaflet-providers/preview/
-    // The following do match the Bootstrap design not too badly:
-    //   - Hydda.Full
-    //   - Thunderforest.Neighbourhood
-    //   - Esri.WorldTopoMap
     var map_provider = "{{ site.conference.location.map.map_provider }}";
     var home_coord = [{{ site.conference.location.map.home_coord }}];
     var default_zoom = {{ site.conference.location.map.default_zoom }};
